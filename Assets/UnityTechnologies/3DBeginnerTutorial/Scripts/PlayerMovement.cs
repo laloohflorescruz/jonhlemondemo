9999a11fc877804d21f7622c10d003e1ad12a802
Lalo Flores <eflores@crudcreativo.com>
2023-11-08 21:23:55 -0800

    Game done

--- Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/PlayerMovement.cs
+++ Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/PlayerMovement.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+
 public class PlayerMovement : MonoBehaviour
 {
     public float turnSpeed = 20f;
-    AudioSource footsteps;
 
     Animator m_Animator;
     Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
 
@@ -13,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
-        footsteps = GetComponent<AudioSource>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     void FixedUpdate()
@@ -28,12 +29,18 @@ public class PlayerMovement : MonoBehaviour
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
-        if (!footsteps.isPlaying)
+
+        if (isWalking)
         {
-            footsteps.Stop();
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
         }
-
-
+        else
+        {
+            m_AudioSource.Stop();
+        }
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
